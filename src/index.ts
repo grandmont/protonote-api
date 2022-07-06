844c5a90ac4027d26a213346fe5e4ab8e319688f
gabepereira <gabriel6562014@gmail.com>
2022-07-06 16:36:56 -0300

    Finish typegraphql setup

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,29 @@
 import "reflect-metadata";
 import { ApolloServer } from "apollo-server";
+import { buildSchema } from "type-graphql";
 import { context } from "./context";
-import { schema } from "./schema";
+import { resolvers } from "../prisma/type-graphql";
 
 const port = process.env.PORT || 3333;
 
-export const server = new ApolloServer({
-  schema,
-  context,
-});
-
-server
-  .listen({
-    port,
-  })
-  .then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
+async function bootstrap() {
+  const schema = await buildSchema({
+    resolvers,
+    validate: false,
   });
+
+  const server = new ApolloServer({
+    schema,
+    context,
+  });
+
+  server
+    .listen({
+      port,
+    })
+    .then(({ url }) => {
+      console.log(`🚀 Server ready at ${url}`);
+    });
+}
+
+bootstrap();
--- src/schema.ts
+++
@@ -1,7 +0,0 @@
-import { buildSchemaSync } from "type-graphql";
-import { resolvers } from "../prisma/type-graphql";
-
-export const schema = buildSchemaSync({
-  resolvers,
-  validate: false,
-});
